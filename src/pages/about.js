96b11220d6f55c757ee39fa704ae81a2ffc65de7
Nikolaj Jensen <nikolaj.app.dev@gmail.com>
2020-08-23 20:36:01 +0200

    Finished adding info in about screen

--- src/pages/about.js
+++ src/pages/about.js
@@ -22,6 +22,9 @@ export default function About() {
                             <div class="progress-bar bg-dark" role="progressbar" style={width90}>Flutter & Dart</div>
                         </div>
                         <div class="progress" style={progHeight}>
+                            <div class="progress-bar bg-dark" role="progressbar" style={width70}>Python</div>
+                        </div>
+                        <div class="progress" style={progHeight}>
                             <div class="progress-bar bg-dark" role="progressbar" style={width70}>HTML</div>
                         </div>
                         <div class="progress" style={progHeight}>
@@ -37,37 +40,47 @@ export default function About() {
                             <div class="progress-bar bg-dark" role="progressbar" style={width80}>Github & Git</div>
                         </div>
                         <div class="progress" style={progHeight}>
-                            <div class="progress-bar bg-dark" role="progressbar" style={width70}>UI Design</div>
+                            <div class="progress-bar bg-dark" role="progressbar" style={width60}>UI Design</div>
                         </div>
                     </div>
                     <div className="col-sm">
                         <h2><b>Who am i?</b></h2>
                         <img className="img-fluid rounded" src={require("../assets/headshot.jpg")} alt="Headshot"></img>
-                        <p className="text-center">A passionated developer that aims to make beautiful yet usefull products.</p>
                     </div>
                     <div className="col-sm">
                         <h2><b>Biologist</b></h2>
                         <div class="progress" style={progHeight}>
-                            <div class="progress-bar bg-dark" role="progressbar" style={width90}>Research</div>
+                            <div class="progress-bar bg-dark" role="progressbar" style={width80}>Zoophysiology</div>
                         </div>
                         <div class="progress" style={progHeight}>
-                            <div class="progress-bar bg-dark" role="progressbar" style={width60}>Laboratory work</div>
+                            <div class="progress-bar bg-dark" role="progressbar" style={width90}>Python, R, MatLab</div>
                         </div>
                         <div class="progress" style={progHeight}>
-                            <div class="progress-bar bg-dark" role="progressbar" style={width60}></div>
+                            <div class="progress-bar bg-dark" role="progressbar" style={width70}>Research</div>
                         </div>
                         <div class="progress" style={progHeight}>
-                            <div class="progress-bar bg-dark" role="progressbar" style={width70}></div>
+                            <div class="progress-bar bg-dark" role="progressbar" style={width70}>Laboratory work</div>
                         </div>
                         <div class="progress" style={progHeight}>
-                            <div class="progress-bar bg-dark" role="progressbar" style={width60}></div>
+                            <div class="progress-bar bg-dark" role="progressbar" style={width80}>Academic writing</div>
                         </div>
                         <div class="progress" style={progHeight}>
-                            <div class="progress-bar bg-dark" role="progressbar" style={width50}></div>
+                            <div class="progress-bar bg-dark" role="progressbar" style={width70}>Experimental design</div>
                         </div>
                         <div class="progress" style={progHeight}>
-                            <div class="progress-bar bg-dark" role="progressbar" style={width50}></div>
+                            <div class="progress-bar bg-dark" role="progressbar" style={width50}>Dissemination</div>
                         </div>
+                    </div>
+                    <div className="col-sm"></div>
+                </div>
+                <div className="row justify-content-md-center">
+                    <div className="col-sm"></div>
+                    <div className="col-sm">
+                        <p className="text-center">Innovative mind with experience working as a developer. Capable of working with a variety of technology and software solutions, and managing databases. Valuable team member who has experience diagnosing problems and developing solutions.</p>
+                    </div>
+                    <div className="col-sm"></div>
+                    <div className="col-sm">
+                        <p className="text-center">Specialized biologist, with experience in dataanalysis in both Python, MatLab, and R. Also capable of doing experimental designs and performing said experiment, with insight into needed changes and improvements.</p>
                     </div>
                     <div className="col-sm"></div>
                 </div>
