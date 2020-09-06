37122d67e26f80ca5ac83a868209e348f7703172
nikolajjsj <nikolaj.app.dev@gmail.com>
2020-09-06 17:02:28 +0200

    Updated some css + bootstrap

--- src/pages/react.jsx
+++ src/pages/react.jsx
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+const style = {
+  display: "flex",
+  flexWrap: "wrap",
+  flexDirection: "row",
+  justifyContent: "center",
+};
+
 export default class Reacts extends Component {
   render() {
     return (
-      <div
-        ref={this.props.reference}
-        className="container-fluid text-center p-5"
-        style={{ backgroundColor: "#F0F0F0" }}
-      >
-        <h1 className="display-4 p-5">REACT PROJECTS</h1>
+      <div className="text-center p-5" style={{ backgroundColor: "#F0F0F0" }}>
+        <h1 className="display-4">REACT PROJECTS</h1>
 
-        <Link className="btn btn-dark" to="/sorting">
-          Sorting visualizer
-        </Link>
+        <div style={style} className="p-5">
+          <Link className="btn btn-dark" to="/sorting">
+            Sorting visualizer
+          </Link>
+        </div>
       </div>
     );
   }
