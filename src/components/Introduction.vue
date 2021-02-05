9ad80f9cfe1c2c7a5880de51a2287d9d8f0670e1
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-02-05 10:46:31 +0100

    Renamed Frontpage to Introduction + merged with About section

---
+++ src/components/Introduction.vue
@@ -0,0 +1,71 @@
+<template>
+  <div className="introduction">
+    <h1 className="introduction--title">
+      Hey, I'm <strong className="text__danger">Nikolaj Jensen</strong>.
+    </h1>
+    <p className="introduction--undertitle">
+      Experienced <strong className="text__danger">developer</strong> as well as
+      specialized <strong className="text__danger">biologist</strong>.
+    </p>
+    <img
+      class="introduction--headshot"
+      src="/assets/headshot.jpg"
+      alt="Headshot"
+    />
+    <p class="introduction--paragraph">
+      Experienced software developer proficient in Dart, JavaScript, and Python.
+      Previous work has been done on frontend development, working with Flutter
+      for hybrid mobile app development. Whereas web development have been done
+      with Vue, React, and Angular. In adition to this i have used SCSS for
+      styling, with the design paradigm being BEM. I also have a education as a
+      biologist, where I've worked mainly within Zoophysiology. This includes
+      laboratory work with several mammals, reptiles, amphibians, and insects.
+      This work has led to a published peer-reviewed article, on insect cold
+      physiology. Dataanalysis has been performed with the use of Python,
+      MatLab, and R.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Introduction',
+};
+</script>
+
+<style scoped>
+.introduction {
+  width: 80%;
+  margin: auto;
+  padding-top: 150px;
+  padding-bottom: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+
+.introduction--title {
+  font-size: 3rem;
+  margin-bottom: 10%;
+}
+
+.introduction--undertitle {
+  font-size: 2rem;
+  margin-bottom: 10%;
+}
+
+.introduction--headshot {
+  height: 10rem;
+  width: 10rem;
+  border-radius: 50%;
+  margin: 1rem;
+  border: 4px solid black;
+}
+
+.introduction--paragraph {
+  margin: 1rem;
+  text-align: justify;
+  font-size: 0.9rem;
+}
+</style>
