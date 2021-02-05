82496f9cee8d9af2f0c98b21b38f9a7d7e72b978
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-02-05 10:47:26 +0100

    Updated App.vue styling

--- src/App.vue
+++ src/App.vue
@@ -23,20 +23,21 @@ export default {
 }
 
 * {
-  color: black;
   font-family: "Montserrat";
+  color: black;
   margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-a {
-  text-decoration: none;
+.text__danger {
+  color: #ca3433;
 }
 </style>
