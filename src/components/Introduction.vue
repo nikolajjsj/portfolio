1f4dcfe173074685c4bcbd23bd4c9f8c38c84c49
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-02-05 11:17:34 +0100

    Updated detail paragraph + changed some CSS

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -14,15 +14,14 @@
     />
     <p class="introduction--paragraph">
       Experienced software developer proficient in Dart, JavaScript, and Python.
-      Previous work has been done on frontend development, working with Flutter
-      for hybrid mobile app development. Whereas web development have been done
-      with Vue, React, and Angular. In adition to this i have used SCSS for
-      styling, with the design paradigm being BEM. I also have a education as a
-      biologist, where I've worked mainly within Zoophysiology. This includes
-      laboratory work with several mammals, reptiles, amphibians, and insects.
-      This work has led to a published peer-reviewed article, on insect cold
-      physiology. Dataanalysis has been performed with the use of Python,
-      MatLab, and R.
+      I've mainly worked on frontend development for both web and mobile.
+      Mobile development has been with either Flutter or some native Android development.
+      Whereas web development have been done with Vue, React, and Angular.
+      In adition to this i have used SCSS and/or CSS for styling, with the design paradigm being BEM.
+      I also have a education as a biologist, where I've worked mainly within Zoophysiology.
+      This includes laboratory work with several mammals, reptiles, amphibians, and insects.
+      This work has led to a published peer-reviewed article, on insect cold physiology.
+      Dataanalysis has been performed with the use of Python, MatLab, and R.
     </p>
   </div>
 </template>
@@ -67,5 +66,11 @@ export default {
   margin: 1rem;
   text-align: justify;
   font-size: 0.9rem;
+}
+
+@media only screen and (min-width: 1024px) {
+  .introduction {
+    width: 50%;
+  }
 }
 </style>
