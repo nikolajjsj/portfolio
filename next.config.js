b3d5db80965139976482b907849b2dc43364fb12
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-11-18 17:52:03 +0100

    Update next config with typed support and image domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['cdn-images-1.medium.com'],
+  },
 }
