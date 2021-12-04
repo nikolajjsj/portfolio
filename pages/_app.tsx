836d71ed01c474749a4d62a9c6efe0e7659ee6fb
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-12-04 17:36:55 +0100

    Remove import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Footer from '../components/Footer'
-import React from 'react'
 import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
