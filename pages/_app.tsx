65e9ee4290bcd35a1f2ac58c7e61d4b28121aeed
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-12-04 17:28:41 +0100

    Change capital

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import React from 'react'
+import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
